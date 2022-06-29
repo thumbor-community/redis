8811313c1e7970dc8dd4b0ff4220bd41394db3a4
maximka777 <maksim.borovskij@yandex.ru>
2022-06-29 16:13:12 +0200

    Fixed Redis cluster wait-healthy script

--- tests/fixtures/redis-cluster/cluster-entrypoint.sh
+++ tests/fixtures/redis-cluster/cluster-entrypoint.sh
@@ -5,39 +5,33 @@ redis-server /redis/redis-cluster-node-0.conf
 redis-server /redis/redis-cluster-node-1.conf
 redis-server /redis/redis-cluster-node-2.conf
 
+# Join nodes into cluster
 echo yes | redis-cli --cluster create 127.0.0.1:6390 127.0.0.1:6391 127.0.0.1:6392 --cluster-replicas 0
 
-# wait until cluster_state is ok
-while true; do
-  echo "$(redis-cli -h localhost -p 6390 CLUSTER INFO 2> /dev/null | grep cluster_state:ok)"
-  if [ "$(redis-cli -h localhost -p 6390 CLUSTER INFO 2> /dev/null | grep cluster_state:ok)" != "" ]; then
-    break
-  fi
-  echo .
-  sleep 1
-done
+# Wait until cluster is up
+for port in 6390 6391 6392; do
+  echo $port
+  while true; do
+    cluster_info=$(redis-cli -h localhost -p $port CLUSTER INFO 2> /dev/null)
 
-# wait until cluster_size is 3
-while true; do
-  echo "$(redis-cli -h localhost -p 6390 CLUSTER INFO 2> /dev/null | grep cluster_size:3)"
-  if [ "$(redis-cli -h localhost -p 6390 CLUSTER INFO 2> /dev/null | grep cluster_size:3)" != "" ]; then
-    break
-  fi
-  echo .
-  sleep 1
-done
+    cluster_state=$(echo "$cluster_info" | grep "cluster_state:ok" | grep -o 'ok')
+    cluster_size=$(echo "$cluster_info" | grep "cluster_size:" | grep -oE '[0-9]+')
 
-# wait until all cluster_slots are ok
-# see "RedisCluster specific options" at https://github.com/redis/redis-py
-while true; do
-  info=$(redis-cli -h localhost -p 6390 CLUSTER INFO 2> /dev/null)
-  slots_assigned=$(echo $info | grep "cluster_slots_assigned:" | grep -oE '[0-9]+')
-  slots_ok=$(echo $info | grep "cluster_slots_ok:" | grep -oE '[0-9]+')
-  if [ "$slots_assigned" == "$slots_ok" ]; then
-    break
-  fi
-  echo .
-  sleep 1
+    # See "RedisCluster specific options" at https://github.com/redis/redis-py#cluster-mode why we check slots.
+    cluster_slots_assigned=$(echo "$cluster_info" | grep "cluster_slots_assigned:" | grep -oE '[0-9]+')
+    cluster_slots_ok=$(echo "$cluster_info" | grep "cluster_slots_ok:" | grep -oE '[0-9]+')
+
+    echo "$cluster_state"
+    echo "$cluster_size"
+    echo "$cluster_slots_assigned"
+    echo "$cluster_slots_ok"
+    if [ "$cluster_state" == "ok" ] && [ "$cluster_size" == "3" ] && [ "$cluster_slots_assigned" == "$cluster_slots_ok" ]; then
+      break
+    fi
+
+    echo .
+    sleep 1
+  done
 done
 
 sleep infinity
