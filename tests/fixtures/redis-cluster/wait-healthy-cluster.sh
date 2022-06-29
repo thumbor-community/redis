d40c2956908569eb7c9995ebdca7c90c8434c166
maximka777 <maksim.borovskij@yandex.ru>
2022-06-29 16:48:35 +0200

    Wait for all nodes to have healthy cluster info

--- tests/fixtures/redis-cluster/wait-healthy-cluster.sh
+++ tests/fixtures/redis-cluster/wait-healthy-cluster.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
 # Wait until cluster is up
-while true; do
-  cluster_info=$(redis-cli -h localhost -p 6390 CLUSTER INFO 2> /dev/null)
+for port in 6390 6391 6392; do
+  echo $port
+  while true; do
+    cluster_info=$(redis-cli -h localhost -p $port CLUSTER INFO 2> /dev/null)
 
-  cluster_state=$(echo "$cluster_info" | grep "cluster_state:ok" | grep -o 'ok')
-  cluster_size=$(echo "$cluster_info" | grep "cluster_size:" | grep -oE '[0-9]+')
+    cluster_state=$(echo "$cluster_info" | grep "cluster_state:ok" | grep -o 'ok')
+    cluster_size=$(echo "$cluster_info" | grep "cluster_size:" | grep -oE '[0-9]+')
 
-  # See "RedisCluster specific options" at https://github.com/redis/redis-py#cluster-mode why we check slots.
-  cluster_slots_assigned=$(echo "$cluster_info" | grep "cluster_slots_assigned:" | grep -oE '[0-9]+')
-  cluster_slots_ok=$(echo "$cluster_info" | grep "cluster_slots_ok:" | grep -oE '[0-9]+')
+    # See "RedisCluster specific options" at https://github.com/redis/redis-py#cluster-mode why we check slots.
+    cluster_slots_assigned=$(echo "$cluster_info" | grep "cluster_slots_assigned:" | grep -oE '[0-9]+')
+    cluster_slots_ok=$(echo "$cluster_info" | grep "cluster_slots_ok:" | grep -oE '[0-9]+')
 
-  if [ "$cluster_state" == "ok" ] && [ "$cluster_size" == "3" ] && [ "$cluster_slots_assigned" == "$cluster_slots_ok" ]; then
-    break
-  fi
+    if [ "$cluster_state" == "ok" ] && [ "$cluster_size" == "3" ] && [ "$cluster_slots_assigned" == "$cluster_slots_ok" ]; then
+      break
+    fi
 
-  echo .
-  sleep 1
+    echo .
+    sleep 1
+  done
 done
